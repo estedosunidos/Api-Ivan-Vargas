2354e394f6165c17431bd6c643c9a06afa6fbb83
Ivan Esteban Vargas Martinez <ivanestebanvargasmartinez@gmail.com>
2023-11-26 20:50:23 -0500

    esta probando la funcionalida clonar pelicula

--- src/domain/dtos/clonamoviesDto.ts
+++ src/domain/dtos/clonamoviesDto.ts
@@ -1,16 +1,17 @@
 export class CloneMovieDto {
   constructor(
     public id: string,
+    public title: string,
   ) {}
 
   static create(params: any): [string?, CloneMovieDto?] {
-    const { id } = params;
+    const { id,title } = params;
 
     // Basic validation: Check if id is a non-empty string
     if (!id || typeof id !== 'string' || id.trim() === '') {
       return ['Invalid id parameter'];
     }
 
-    return [undefined, new CloneMovieDto(id)];
+    return [undefined, new CloneMovieDto(id,title)];
   }
 }
